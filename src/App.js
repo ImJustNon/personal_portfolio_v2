9527e2132f2a84c40936c3d1cc053accd9f30b0e
Kanakorn Thaiprakhon <Im_Just_Non@hotmail.com>
2023-11-15 15:54:15 +0700

    Feat : Add important package and setup it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
+import { AnimatePresence, motion } from 'framer-motion';
+import { BrowserRouter, Routes, Route, redirect } from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  	return (
+		<ChakraProvider>
+			<Routes>
+				<Route path='/' element={<Home />} />
+			</Routes>
+		</ChakraProvider>
+  	);
 }
 
 export default App;
