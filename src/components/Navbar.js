cff27b3995aaf672870d1a0c19c94465b1c3add9
♻ NOr._Nor ♻ <Im_Just_Non@hotmail.com>
2023-12-03 04:49:12 +0700

    Feat : Fixed Mobile Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,42 +11,56 @@ import {
     useDisclosure,
     Input,
     Button
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
 import React from "react";
+import { useLocation } from "react-router-dom";
+import enImage from "../assets/images/language/en.jpg";
+import thImage from "../assets/images/language/th.jpg";
 
 function Navbar({language}){
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+    const { pathname } = useLocation();
     function HandleToggleLanguage(lang){
-        localStorage.setItem("config_language", lang);
-        window.location.reload();
+        if(pathname.startsWith(`/${lang}`)) return; // return if choose language same as current language
+
+        localStorage.setItem("config_language", lang); // save to localstorage
+        window.location.replace(pathname.startsWith("/en") ? pathname.replace("en", "th") : pathname.replace("th", "en")); // reload page with same page
     }
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
     return(
         <>
             <div className="navbar fixed text-white z-10" style={{backgroundColor: "rgba(0, 0, 0, 0.7)"}}>
                 <div className="container mx-auto">
                     <div className="navbar-start">
-                        <Link to={"/"} className="text-xl ml-5">{"< NOr._Nor />"}</Link>
+                        <Link to={`/${language}`} className="text-xl ml-5">{"< NOr._Nor />"}</Link>
                     </div>
                     <div className="navbar-center hidden lg:flex">
                         <div className="menu menu-horizontal px-1 space-x-3">
                             <Link to={`/${language}/`} className="btn btn-sm font-normal btn-ghost text-white" >
+                                <i className="fa-solid fa-house"></i>
                                 {language === "en" ? "Home" : "หน้าหลัก"}
                             </Link>
                             <Link to={`/${language}/personal-history`} className="btn btn-sm font-normal btn-ghost text-white" >
+                                <i className="fa-solid fa-folder"></i>
                                 {language === "en" ? "Personal History" : "ประวัติส่วนตัว"}
                             </Link>
                             <Link to={`/${language}/certificates`} className="btn btn-sm font-normal btn-ghost text-white" >
+                                <i className="fa-solid fa-trophy"></i>
                                 {language === "en" ? "Certificates" : "เกียรติบัตร"}
                             </Link>
                             <Link to={`/${language}/activities`} className="btn btn-sm font-normal btn-ghost text-white" >
+                                <i className="fa-solid fa-medal"></i>
                                 {language === "en" ? "Activities" : "กิจกรรม"}
                             </Link>
                             <Link to={`/${language}/projects`} className="btn btn-sm font-normal btn-ghost text-white" >
+                                <i className="fa-solid fa-code"></i>
                                 {language === "en" ? "Projects" : "โปรเจค"}
                             </Link>
                             <Link to={`/${language}/socials`} className="btn btn-sm font-normal btn-ghost text-white" >
+                                <i className="fa-solid fa-share-from-square"></i>
                                 {language === "en" ? "Socials" : "โซเชียล"}
                             </Link>
                         </div>
@@ -57,29 +71,31 @@ function Navbar({language}){
                                 {
                                     language === "en" ?
                                         <>
-                                            <img src="/language/en.jpg" className="h-3.5 w-3.5 object-cover inline rounded-full mr-1" ></img> 
+                                            <img alt="en_image" src={enImage} className="h-3.5 w-3.5 object-cover inline rounded-full mr-1" />
                                             English (US) 
                                         </> 
                                         : 
                                         <>
-                                            <img src="/language/th.jpg" className="h-3.5 w-3.5 object-cover inline rounded-full mr-1" ></img> 
+                                            <img alt="th_image" src={thImage} className="h-3.5 w-3.5 object-cover inline rounded-full mr-1" />
                                             ภาษาไทย (TH)
                                         </>
                                 }
                             </div>
                             <div tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-md w-max text-white space-y-3" style={{backgroundColor: "rgba(0, 0, 0, 0.7)"}} >
                                 <button className="text-left" onClick={() => HandleToggleLanguage("en")}>
-                                    <img src="/language/en.jpg" className="h-3.5 w-3.5 object-cover inline rounded-full mr-2" ></img> English (US)
+                                    <img alt="en_image" src={enImage} className="h-3.5 w-3.5 object-cover inline rounded-full mr-2" />English (US)
                                 </button>
                                 <button className="text-left" onClick={() => HandleToggleLanguage("th")}>
-                                    <img src="/language/th.jpg" className="h-3.5 w-3.5 object-cover inline rounded-full mr-2" ></img> ภาษาไทย (TH)
+                                    <img alt="th_image" src={thImage} className="h-3.5 w-3.5 object-cover inline rounded-full mr-2" />ภาษาไทย (TH) 
                                 </button>
                             </div>
                         </div>
-                        <div className="btn btn-ghost lg:hidden" onClick={() => setIsMenuOpen(prev => prev === true ? false : true)}>
-                            <i className="fa-solid fa-bars fa-lg"></i>    
+
+                        {/* Mobile burger button */}
+                        <div className="btn btn-ghost lg:hidden" onClick={onOpen}>
+                            <Burger />
                         </div>
-                        
+                        <MobileDrawer isOpen={isOpen} onClose={onClose} onOpen={onOpen} language={language} />
                     </div>
                 </div>
             </div>
@@ -88,40 +104,126 @@ function Navbar({language}){
 }
 
 
-function MobileDrawer() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+function Burger(){
+    return(
+        <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            className="h-5 w-5" 
+            fill="none" 
+            viewBox="0 0 24 24" 
+            stroke="currentColor"
+        >
+            <path 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                strokeWidth="2" 
+                d="M4 6h16M4 12h8m-8 6h16" 
+            />
+        </svg> 
+    );
+}
+
+
+function MobileDrawer({isOpen, onClose, onOpen, language}) {
     const btnRef = React.useRef()
   
     return (
-      <>
-        <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
-          Open
-        </Button>
-        <Drawer
-          isOpen={isOpen}
-          placement='right'
-          onClose={onClose}
-          finalFocusRef={btnRef}
-        >
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>Create your account</DrawerHeader>
-  
-            <DrawerBody>
-              <Input placeholder='Type here...' />
-            </DrawerBody>
-  
-            <DrawerFooter>
-              <Button variant='outline' mr={3} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button colorScheme='blue'>Save</Button>
-            </DrawerFooter>
-          </DrawerContent>
-        </Drawer>
-      </>
-    )
+        <>
+            <Drawer
+                isOpen={isOpen}
+                placement='bottom'
+                onClose={onClose}
+                finalFocusRef={btnRef}
+                size={"xs"}
+                isFullHeight={false}
+            >
+                <DrawerOverlay />
+                <DrawerContent>
+                    
+                    <DrawerHeader>
+                        <div className="text-left flex flex-row justify-around">
+                            <div className="text-2xl">
+                                {language === "en" ? "< N a v i g a t i o n />" : "< เ ม นู />"} 
+                            </div>
+                        </div>
+                    </DrawerHeader>
+        
+                    <DrawerBody>
+                        <div className="px-5 space-y-1">
+
+                            <Link to={`/${language}/`} className="btn btn-md btn-neutral w-full">
+                                <div className="grid grid-cols-3 gap-4 w-full">
+                                    <div class="text-center">
+                                        <i className="fa-solid fa-house"></i>
+                                    </div>
+                                    <div class="col-span-2 text-left">
+                                        {language === "en" ? "Home" : "หน้าหลัก"}
+                                    </div>
+                                </div>
+                            </Link>
+                            <Link to={`/${language}/personal-history`} className="btn btn-md btn-neutral w-full">
+                                <div className="grid grid-cols-3 gap-4 w-full">
+                                    <div class="text-center">
+                                        <i className="fa-solid fa-folder"></i>
+                                    </div>
+                                    <div class="col-span-2 text-left">
+                                        {language === "en" ? "Personal History" : "ประวัติส่วนตัว"}
+                                    </div>
+                                </div>
+                            </Link>
+                            <Link to={`/${language}/certificates`} className="btn btn-md btn-neutral w-full">
+                                <div className="grid grid-cols-3 gap-4 w-full">
+                                    <div class="text-center">
+                                        <i className="fa-solid fa-trophy"></i>
+                                    </div>
+                                    <div class="col-span-2 text-left">
+                                        {language === "en" ? "Certificates" : "เกียรติบัตร"}
+                                    </div>
+                                </div>
+                            </Link>
+                            <Link to={`/${language}/activities`} className="btn btn-md btn-neutral w-full">
+                                <div className="grid grid-cols-3 gap-4 w-full">
+                                    <div class="text-center">
+                                        <i className="fa-solid fa-medal"></i>
+                                    </div>
+                                    <div class="col-span-2 text-left">
+                                        {language === "en" ? "Activities" : "กิจกรรม"}
+                                    </div>
+                                </div>
+                            </Link>
+                            <Link to={`/${language}/projects`} className="btn btn-md btn-neutral w-full">
+                                <div className="grid grid-cols-3 gap-4 w-full">
+                                    <div class="text-center">
+                                        <i className="fa-solid fa-code"></i>
+                                    </div>
+                                    <div class="col-span-2 text-left">
+                                        {language === "en" ? "Projects" : "โปรเจค"}
+                                    </div>
+                                </div>
+                            </Link>
+                            <Link to={`/${language}/socials`} className="btn btn-md btn-neutral w-full">
+                                <div className="grid grid-cols-3 gap-4 w-full">
+                                    <div class="text-center">
+                                        <i className="fa-solid fa-share-from-square"></i>
+                                    </div>
+                                    <div class="col-span-2 text-left">
+                                        {language === "en" ? "Socials" : "โซเชียล"}
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
+                    </DrawerBody>
+        
+                    <DrawerFooter>
+                        <button className="btn btn-sm rounded-full" onClick={onClose}>
+                            <i className="fa-solid fa-x"></i> 
+                            {language === "en" ? "Close" : "ปิดเมนู"}
+                        </button>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+        </>
+    );
   }
 
 
--- src/layouts/AppLayout.js
+++ src/layouts/AppLayout.js
@@ -2,21 +2,43 @@ import Navbar from "../components/Navbar";
 import { useParams, useNavigate } from 'react-router-dom';
 import React from "react";
 // import '../App.css'
+import { AnimatePresence, motion } from 'framer-motion';
+import { useLocation } from 'react-router-dom';
 
 function AppLayout({ children }){
     const { language } = useParams();
+    const { pathname } = useLocation();
+
 
     return(
         <>
             <Navbar language={language} />
-
-            {React.Children.map(children, (child) =>{
-                return React.cloneElement(child, { 
-                    language: language,
-                });
-            })}
-
-            
+            <AnimatePresence wait>
+                <motion.div 
+                    key={pathname}
+                    initial="initialState"
+                    animate="animateState"
+                    exit="exitState"
+                    transition={{
+                        duration: 0.3,
+                    }}
+                    variants={{
+                        initialState: {
+                            opacity: 0, transform: "translateY(150px)",
+                        },
+                        animateState: {
+                            opacity: 1, transform: 0,
+                        },
+                        exitState: {},
+                    }}
+                >
+                    {React.Children.map(children, (child) =>{
+                        return React.cloneElement(child, { 
+                            language: language,
+                        });
+                    })}
+                </motion.div>
+			</AnimatePresence>
         </>
     );
 }
