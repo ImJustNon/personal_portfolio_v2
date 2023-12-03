8d9f48ba7a95675985ce834c6b665780133e074f
♻ NOr._Nor ♻ <Im_Just_Non@hotmail.com>
2023-12-03 19:45:48 +0700

    Feat : Fixed Loading layput

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,87 @@
+
 export const config = {
-    address: "http://127.0.0.1"
+    address: "http://127.0.0.1",
+    api: {
+        lanyard: {
+            discordUserId: "708965153131200594",
+        },
+        nonlnwzaPortfolio: {
+            key: process.env.REACT_APP_PORTFOLIO_API_KEY,
+        },
+        github: {
+            username: "imjustnon"
+        }
+    },
+    data: {
+        pages: {
+            social: {
+                defaultClass: "btn btn-ghost btn-outline mx-1 my-1 text-black",
+                buttons: [
+                    {
+                        name: "Twitter",
+                        url: "https://twitter.com/NoNnYhA55",
+                        iconClass: "fa-brands fa-twitter",
+                        hoverClass: "btn btn-active btn-info mx-1 my-1 text-white",
+                    },
+                    {
+                        name: "Youtube",
+                        url: "https://www.youtube.com/channel/UCCAlDdKswolIsOERlt0QWbw",
+                        iconClass: "fa-brands fa-youtube",
+                        hoverClass: "btn btn-active btn-error mx-1 my-1 text-white",
+                    },
+                    {
+                        name: "Github",
+                        url: "https://github.com/ImJustNon",
+                        iconClass: "fa-brands fa-github",
+                        hoverClass: "btn btn-active btn-neutral mx-1 my-1 text-white",
+                    },
+                    {
+                        name: "Steam",
+                        url: "https://steamcommunity.com/profiles/76561198980838273",
+                        iconClass: "fa-brands fa-steam",
+                        hoverClass: "btn btn-active btn-neutral mx-1 my-1 text-white",
+                    },
+                    {
+                        name: "Twitch",
+                        url: "https://www.twitch.tv/im_just_non",
+                        iconClass: "fa-brands fa-twitch",
+                        hoverClass: "btn btn-active btn-secondary mx-1 my-1 text-white",
+                    },
+                    {
+                        name: "Email",
+                        url: "mailto:Not.Non_006@outlook.sg",
+                        iconClass: "fa-solid fa-at",
+                        hoverClass: "btn btn-active btn-accent mx-1 my-1 text-white",
+                    },
+                    {
+                        name: "Discord",
+                        url: "https://discord.com/users/708965153131200594",
+                        iconClass: "fa-brands fa-discord",
+                        hoverClass: "btn btn-active btn-primary mx-1 my-1 text-white",
+                    },
+                    {
+                        name: "Spotify",
+                        url: "https://open.spotify.com/user/mvym46mna0nhjjvdt64l7m7uj",
+                        iconClass: "fa-brands fa-spotify",
+                        hoverClass: "btn btn-active btn-success mx-1 my-1 text-white",
+                    },
+                ],
+                webPageButton: [
+                    {
+                        name: "Spotify",
+                        url: "https://open.spotify.com/user/mvym46mna0nhjjvdt64l7m7uj",
+                        iconClass: "fa-brands fa-spotify",
+                        hoverClass: "btn btn-active btn-success mx-1 my-1 text-white",
+                    },
+                ]
+            },
+            project: {
+                group: [
+                    "m3",
+                    "voc_cert_1",
+                    "voc_cert_2"
+                ],
+            },
+        }
+    }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import { Container, VStack, Box } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import { useLocation, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { config } from "../config/config";
 
 function Home({ language }){
     const { pathname } = useLocation();
@@ -15,7 +16,7 @@ function Home({ language }){
 
 
 	useEffect(() =>{
-		fetch(`https://api.github.com/users/imjustnon`).then(response => response.json()).then(response =>{
+		fetch(`https://api.github.com/users/${config.api.github.username}`).then(response => response.json()).then(response =>{
 			setGithubProfileURL(response.avatar_url);
 			setGithubProfileUserName(response.name);
 			setGithubInfo(response);
@@ -25,75 +26,75 @@ function Home({ language }){
 
     return (
         <>
-        <div className='container mx-auto'>
-            <div className='mt-10 mx-auto w-[20rem] md:w-[45rem] noselect'>
-                <div className="hero bg-white bg-opacity-40 rounded-2xl text-center md:text-start text-black">
-                    <div className="hero-content flex-col md:flex-row">
-                        {isLoaded ? 
-                            <div className="w-2/3 md:h-auto">
-                                <img src={githubProfileURL} className="w-full rounded-lg shadow-2xl animate__animated animate__fadeIn" />
+            <div className='container mx-auto'>
+                <div className='mt-10 mx-auto w-[20rem] md:w-[45rem] noselect'>
+                    <div className="hero bg-white bg-opacity-40 rounded-2xl text-center md:text-start text-black">
+                        <div className="hero-content flex-col md:flex-row">
+                            {isLoaded ? 
+                                <div className="w-2/3 md:h-auto">
+                                    <img src={githubProfileURL} className="w-full rounded-lg shadow-2xl animate__animated animate__fadeIn" />
+                                </div>
+                                :
+                                <div className="h-48 w-2/3 text-center flex justify-center md:h-52">
+                                    <span className="loading loading-spinner loading-lg"></span>
+                                </div>
+                            }
+                            <div>
+                                <h1 className={`text-2xl font-semibold `}>
+                                    {isLoaded ? 
+                                        <a className="animate__animated animate__fadeIn" href="https://github.com/ImJustNon" target="_blank">{`< ${githubProfileUserName} />`}</a>
+                                        : 
+                                        <div className="text-center md:w-44">
+                                            <span className="loading loading-spinner loading-md"></span>
+                                        </div>
+                                    }
+                                </h1>
+                                <p className="py-3">I call myself a Dev. But I hate code. LOL.</p>
+                                <p className="py-3">Currently studying at the Vocational Certificate at the Science-Based Technology Vocational College (Chonburi). Information Technology branch.</p>
                             </div>
-                            :
-                            <div className=" h-48 w-2/3 text-center md:h-auto">
-                                <span className="loading loading-spinner loading-lg"></span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='mt-10 md:px-56'>
+                    <div className='mx-auto'>
+                        <h1 className='text-center text-3xl font-semibold'>About Me</h1>
+                    </div>
+                    <div className="mt-5 mx-auto hero bg-white bg-opacity-40 rounded-2xl text-black py-8 px-8 w-96 md:text-start md:w-auto">
+                        <div className='flex flex-col md:flex-row justify-around w-full'>
+                            <div className='text-black md:w-1/2'>
+                                <h1 className='text-xl text-center mb-3'>
+                                    <i className="fa-solid fa-code"></i> 
+                                    | Dev. Stacks
+                                </h1>
+                                <ul className='space-y-1'>
+                                    <li className="py-2 text-sm font-thin">▪ Programming languages: JavaScript, HTML, CSS, SQL, JSX, Python, C++, EJS </li>
+                                    <li className="py-2 text-sm font-thin">▪ Frameworks: Express.js, React.js, Node.js, Discord.js, Bootstrap, TailwindCSS</li>
+                                    <li className="py-2 text-sm font-thin">▪ Tools: Git</li>
+                                    <li className="py-2 text-sm font-thin">▪ IDEs: Visual Studio Code</li>
+                                    <li className="py-2 text-sm font-thin">▪ Etc: null</li>
+                                </ul>
                             </div>
-                        }
-                        <div>
-                            <h1 className={`text-2xl font-semibold `}>
-                                {isLoaded ? 
-                                    <a className="animate__animated animate__fadeIn" href="https://github.com/ImJustNon" target="_blank">{`< ${githubProfileUserName} />`}</a>
-                                    : 
-                                    <div className="text-center md:w-44">
-                                        <span className="loading loading-spinner loading-md"></span>
-                                    </div>
-                                }
-                            </h1>
-                            <p className="py-3">I call myself a Dev. But I hate code. LOL.</p>
-                            <p className="py-3">Currently studying at the Vocational Certificate at the Science-Based Technology Vocational College (Chonburi). Information Technology branch.</p>
+                            <div className='text-black mt-8 md:w-1/2 md:mt-0'>
+                                <h1 className='text-xl text-center mb-3'>
+                                    🥰 | Personal Information
+                                </h1>
+                                <ul className='space-y-1'>
+                                    <li className="py-2 text-sm font-thin">▪ Live in : Samut Prakan, Thailand</li>
+                                    <li className="py-2 text-sm font-thin">▪ Study At : Science-Based Technology Vocational College (Chonburi)</li>
+                                    <li className="py-2 text-sm font-thin">▪ Field of Study : Information Technology (IT)</li>
+                                    <li className="py-2 text-sm font-thin">▪ Hobbies: Listening to music, watching anime, coding, sleeping</li>
+                                    <li className="py-2 text-sm font-thin">▪ Fav Anime : 86, Spy X Family, Bloom into You, YourName ETC.</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div className='mt-10 md:px-56'>
-                <div className='mx-auto'>
-                    <h1 className='text-center text-3xl font-semibold'>About Me</h1>
-                </div>
-                <div className="mt-5 mx-auto hero bg-white bg-opacity-40 rounded-2xl text-black py-8 px-8 w-96 md:text-start md:w-auto">
-                    <div className='flex flex-col md:flex-row justify-around w-full'>
-                        <div className='text-black md:w-1/2'>
-                            <h1 className='text-xl text-center mb-3'>
-                                <i className="fa-solid fa-code"></i> 
-                                | Dev. Stacks
-                            </h1>
-                            <ul className='space-y-1'>
-                                <li className="py-2 text-sm font-thin">▪ Programming languages: JavaScript, HTML, CSS, SQL, JSX, Python, C++, EJS </li>
-                                <li className="py-2 text-sm font-thin">▪ Frameworks: Express.js, React.js, Node.js, Discord.js, Bootstrap, TailwindCSS</li>
-                                <li className="py-2 text-sm font-thin">▪ Tools: Git</li>
-                                <li className="py-2 text-sm font-thin">▪ IDEs: Visual Studio Code</li>
-                                <li className="py-2 text-sm font-thin">▪ Etc: null</li>
-                            </ul>
-                        </div>
-                        <div className='text-black mt-8 md:w-1/2 md:mt-0'>
-                            <h1 className='text-xl text-center mb-3'>
-                                🥰 | Personal Information
-                            </h1>
-                            <ul className='space-y-1'>
-                                <li className="py-2 text-sm font-thin">▪ Live in : Samut Prakan, Thailand</li>
-                                <li className="py-2 text-sm font-thin">▪ Study At : Science-Based Technology Vocational College (Chonburi)</li>
-                                <li className="py-2 text-sm font-thin">▪ Field of Study : Information Technology (IT)</li>
-                                <li className="py-2 text-sm font-thin">▪ Hobbies: Listening to music, watching anime, coding, sleeping</li>
-                                <li className="py-2 text-sm font-thin">▪ Fav Anime : 86, Spy X Family, Bloom into You, YourName ETC.</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+            <div className='mt-10 px-56'>
+            
             </div>
-		</div>
-
-        <div className='mt-10 px-56'>
-           
-        </div>
 
         </>
     );
