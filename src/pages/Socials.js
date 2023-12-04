814cb6855505c5046e80e6a7ca2eb965bb8f9fa6
♻ NOr._Nor ♻ <Im_Just_Non@hotmail.com>
2023-12-05 00:03:28 +0700

    Feat : Fixed margin b

--- src/pages/Socials.js
+++ src/pages/Socials.js
@@ -27,7 +27,7 @@ function Socials({ language }){
                         {language === "en" ? "Socials" : "โซเชียล"}
                     </h1>
                 </div>
-                <div className="mx-auto w-96 hero bg-white bg-opacity-40 mt-14 py-8 px-8 rounded-2xl md:w-[50rem]">
+                <div className="mx-auto w-96 hero bg-white bg-opacity-40 mt-14 py-8 px-8 mb-5 rounded-2xl md:w-[50rem]">
 
                     <div class="grid grid-cols-1 mx-auto justify-around gap-5 md:grid-cols-3 w-full">
                         {config.data.pages.social.buttons.map((data, i) =>(
