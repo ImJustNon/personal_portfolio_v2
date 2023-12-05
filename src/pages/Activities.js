a40d2631c9eda90114761d7654b32a82a40c9608
♻ NOr._Nor ♻ <Im_Just_Non@hotmail.com>
2023-12-05 19:27:53 +0700

    Feat : Add information to activities page and remove card components

--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -148,22 +148,4 @@ function Activities({ language }){
 }
 
 
-function CardComponent(){
-    return (<>
-        <div className="card card-compact bg-base-100 shadow-2xl">
-            <figure>
-                <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title mx-auto">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary btn-sm">Buy Now</button>
-                </div>
-            </div>
-        </div>
-    </>);
-}
-
-
 export default Activities;
