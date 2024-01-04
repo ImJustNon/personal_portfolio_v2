1ca66dc257fc67814f3d40638ca1bf9d1cfeda8d
♻ NOr._Nor ♻ <Im_Just_Non@hotmail.com>
2024-01-05 04:21:34 +0700

    Feat : fixed translation

--- src/config/translation/activities/data.js
+++ src/config/translation/activities/data.js
@@ -3,10 +3,12 @@ export const activitiesTranslation = {
         "Activities" : "Activities",
         "Vocational Certificate 1st Years": "Vocational Certificate 1st Years",
         "Vocational Certificate 2st Years" : "Vocational Certificate 2st Years",
+        "Details": "Details",
     },
     th: {
         "Activities" : "กิจกรรม",
         "Vocational Certificate 1st Years": "ปวช. 1",
         "Vocational Certificate 2st Years" : "ปวช. 2",
+        "Details": "รายระเอียด",
     }
 }
